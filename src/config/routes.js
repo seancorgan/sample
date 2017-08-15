e859a12ef09aff85b38e368a2d8476b865993d47
Sean Corgan <sean.corgan@gisinc.com>
2017-08-15 14:03:29 -0700

    Add profiles to route

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,11 +4,13 @@ import CounterPage from '../containers/CounterPage';
 import Login from '../containers/Login';
 import AnotherPage from '../containers/AnotherPage';
 import NotFoundPage from '../containers/NotFoundPage';
+import ProfilePage from '../containers/ProfilePage';
 
 export default (
   <Switch>
     <Route exact path="/" component={CounterPage} />
     <Route exact path="/login" component={Login} />
+    <Route exact path="/profile" component={ProfilePage} />
     <Route exact path="/another" component={AnotherPage} />
     <Route component={NotFoundPage} />
   </Switch>
